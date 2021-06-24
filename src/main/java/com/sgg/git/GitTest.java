5a36d5600b0f1ddc03617fd3f3ae1538a6d729ea
zeroleo <zeroleo@qq.com>
2021-06-24 14:57:22 +0800

    hot-fix commit two

--- src/main/java/com/sgg/git/GitTest.java
+++ src/main/java/com/sgg/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hotfix");
+        System.out.println("hotfix change");
+
     }
 }
